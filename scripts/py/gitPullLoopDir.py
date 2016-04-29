6148cbef0fe3ba303b95dea088f8efa6ba82e4e8
Guilherme Botossi <guilherme.botossi_indra@inpe.br>
2016-04-29 16:32:12 -0300

    Adicionando Scripts de python

---
+++ scripts/py/gitPullLoopDir.py
@@ -0,0 +1,11 @@
+import os, sys
+
+root = sys.argv[1]
+folderStartsWith = sys.argv[2]
+
+for item in os.listdir(root):
+	if(item.startswith(folderStartsWith)):
+		curDir = os.path.join(root,item)
+		command = "git --git-dir="+curDir+"\.git --work-tree="+curDir+ " pull origin master"
+		print (command)
+		os.system(command)
