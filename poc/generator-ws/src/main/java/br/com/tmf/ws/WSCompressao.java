d338e983ba85ed8e9d092635467e0391802a26b3
Guilherme Botossi <guilherme.botossi_indra@inpe.br>
2016-07-18 16:33:24 -0300

    Projeto e grafico do highcharts

--- poc/generator-ws/src/main/java/br/com/tmf/ws/WSCompressao.java
+++ poc/generator-ws/src/main/java/br/com/tmf/ws/WSCompressao.java
@@ -1,6 +1,12 @@
 package br.com.tmf.ws;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
@@ -49,6 +55,38 @@ public class WSCompressao {
         
         return outputString;
 	 }
+	
+	public static void main(String[] args) throws Exception {
+		List<String> lista = new ArrayList<String>();
+		
+		lista.add("E:\\scripts\\20160224_000000_Ic_1k.jpg");
+		lista.add("E:\\scripts\\20160224_000000_Ic_flat_1k.jpg");
+		lista.add("E:\\scripts\\20160224_000000_M_1k.jpg");
+		lista.add("E:\\scripts\\20160224_000000_M_color_1k.jpg");
+		lista.add("E:\\scripts\\hmi.Ic_720s.20160214_000000_TAI.1.continuum.fits");
+		lista.add("E:\\scripts\\hmi.m_720s.20160412_000000_TAI.magnetogram.fits");
+		
+		for(String str : lista) {
+			Path path = Paths.get(str);
+			byte[] data = Files.readAllBytes(path);
+			WSCompressao.comprime(data);
+		}
+		
+		
+	}
+	
+	private static void comprime(byte[] input){
+		
+        // Compress the bytes
+        byte[] output = new byte[input.length];
+        Deflater compresser = new Deflater();
+        compresser.setInput(input);
+        compresser.finish();
+        int compressedDataLength = compresser.deflate(output);
+        compresser.end();
+        
+        System.out.println("Tamanho Comprimido = " + compressedDataLength);
+	} 
 
 	private static final Charset CHARSET = Charset.forName("UTF-8");
 }
