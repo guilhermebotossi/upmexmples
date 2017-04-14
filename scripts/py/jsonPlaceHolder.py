b96abf3670ddb9d6d65d899f5bbdf7d3faede2ea
Guilherme Botossi <guilherme.botossi_indra@inpe.br>
2017-04-13 21:18:02 -0300

    poc

--- scripts/py/jsonPlaceHolder.py
+++ scripts/py/jsonPlaceHolder.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import json
 
 with open('enviroments.json') as json_data:
 	d = json.load(json_data)
 	print "Ambientes Disponiveis : " 
+	my_envs = d["enviroments"]
 
-	for x in d["enviroments"] :
-		print x["env"]
+	for x in range(0,len(my_envs)) :
+		print str(x) + " - " + str(my_envs[x]["env"])
+
+
+	mode = int(raw_input("\n\nSelecione um dos ambientes Disponiveis(0-"+str(len(my_envs) - 1)+") : "))
+	print "Serão utilizados os dados de configuração do ambiente de " + my_envs[mode]["env"]
